bfdcaf3fa6d7073ee9ab5fadf2a4d0e232303650
Philippe Assis <assis@philippeassis.com>
2017-11-05 08:08:43 -0200

    es2015, fixed bugs

--- test.js
+++ test.js
@@ -2,22 +2,22 @@ var flags = require('./lib/simpleFlags')
 
 // Default options
 var options = {
-    "args": ["author", "website"],
-    "coffee": false,
-    "not": true,
-    "developer": ['d', 'dev'],
-    "country": {
-        short: 'c',
-        value: "Do not be 'reaça'"
-    }
+  'args': ['author', 'website'],
+  'coffee': false,
+  'not': true,
+  'developer': null,
+  'country': {
+    aliases: ['c'],
+    default: "Do not be 'reaça'"
+  }
 }
 
-console.log("Default options:")
+console.log('Default options:')
 console.log(options)
 
-console.log("\nYou used: \n" + process.argv.slice(2).map(function(value) {
-    return (value.search(/ /g) > -1) ? '"' + value + '"' : value
-}).join(" ")+"\n")
+console.log('\nYou used: \n' + process.argv.slice(2).map(function (value) {
+  return (value.search(/ /g) > -1) ? '"' + value + '"' : value
+}).join(' ') + '\n')
 
-console.log("Result:")
+console.log('Result:')
 console.log(flags(options))
